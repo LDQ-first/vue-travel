de54809b975bdeb18cd2c63ddd5d158e87cef008
LDQ-first <178848890@qq.com>
2017-10-17 16:38:51 +0800

    'optimize'

--- src/views/view.vue
+++ src/views/view.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="view">
-        
+        <ul class="viewlists">
+            <li v-for="(item,index) in view" key="index" class="viewlist">
+                <img :src="item.pic_url" alt="" class="img">
+                <div class="detail">
+                    <h3 class="title">{{item.sname}}</h3>
+                    <p class="desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -20,7 +28,7 @@
             
         },
         computed: {
-            
+
         },
         methods: {
 
@@ -32,6 +40,31 @@
 
 <style lang="scss" scoped>
     .view {
-        
+        background: #CCC;
+        .viewlists {
+            .viewlist {
+                background: #FFF;
+                margin: 0.5em;
+                padding: 0.5em;
+                display: flex;
+                align-items: center;
+                .img {
+                    width: 121px;
+                    height: 90px;
+                    margin-right: 0.5em;
+                }
+                .detail {
+                    flex: 1;
+                    .title {
+                        font-size: 1em;
+                    }
+                    .desc {
+                        /*height: 90px;
+                        overflow: hidden;
+                        text-overflow: ellipsis;*/
+                    }
+                }
+            }
+        }
     }
 </style>
