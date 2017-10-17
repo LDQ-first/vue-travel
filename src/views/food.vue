b12ed4c3005ae2f81b9272e06d504eadf436fe34
LDQ-first <178848890@qq.com>
2017-10-17 16:54:26 +0800

    'optimize'

--- src/views/food.vue
+++ src/views/food.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="food">
-        
+        <ul class="foodlists">
+            <li v-for="(item, index) in food" key="index" class="foodlist">
+                <div class="intro">
+                    <h3 class="title">{{item.name}}</h3>
+                    <div class="detail">
+                        <img :src="`//gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/lvpics/pic/item/${item.pic_url}.jpg`" alt="" class="img">
+                        <p class="desc">{{item.desc}}</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -32,6 +42,31 @@
 
 <style lang="scss" scoped>
     .food {
-        
+        background: #CCC;
+        .foodlists {
+            .foodlist {
+                background: #FFF;
+                margin: 0.5em;
+                padding: 0.5em;
+                .intro {
+                    .title {
+                        margin-bottom: 1em;
+                    }
+                    .detail {
+                        display: flex;
+                        align-items: center;
+                        .img {
+                            width: 150px;
+                            height: 100px;
+                            margin-right: 0.5em;
+                        } 
+                        .title {
+
+                        }
+                    }
+
+                }
+            }
+        }
     }
 </style>
