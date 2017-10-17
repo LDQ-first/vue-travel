73717f3f1081a1e8ef8c99cc1909d8d5ba6c2f2a
LDQ-first <178848890@qq.com>
2017-10-17 18:42:20 +0800

    'optimize'

--- src/api/index.js
+++ src/api/index.js
@@ -21,8 +21,12 @@ https://lvyou.baidu.com/destination/ajax/webapp/hotel/list?sid=${sid}&rn=10&pn=0
 export const traffic = (sid) => `${proxy}
 https://lvyou.baidu.com/destination/ajax/getroute?sid=${sid}&route=traffic.remote&format=ajax`
 
+/*export const routes = (sid) => `${proxy}
+https://lvyou.baidu.com/plan/webapp/list?sids=${sid}&rn=15&pn=0&t=1508216901368`*/
+
 export const routes = (sid) => `${proxy}
-https://lvyou.baidu.com/plan/webapp/list?sids=${sid}&rn=15&pn=0&t=1508216901368`
+https://lvyou.baidu.com/plan/webapp/list?sids%5B%5D=${sid}&rn=15&pn=0&t=${new Date().getTime()}`
+
 
 export const route = (plid, n) => `${proxy}
 https://lvyou.baidu.com/plan/webapp/newdetail?pl_id=${plid}&from_day=0&days=${n}`
--- src/views/routes.vue
+++ src/views/routes.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="routes">
-        
+        <ul class="routeslists">
+            <li class="routeslist" v-for="(item, index) in routes" key="index">
+                <div class="detail">
+                    <img :src="item.pic_url" alt="" class="img">
+                    <div class="desc">
+                        <h3 class="title">{{item.pl_name}}</h3>
+                        <span class="days">{{item.days}}天</span>
+                        <span class="nickname">{{item.counselor.nickname}}</span>
+                    </div>
+                </div>
+                <ul class="destinations">
+                    <li class="destination" v-for="(destination, i) in item.destinations" key="i" v-if="i <=1">
+                        {{destination.sname}} ->
+                    </li>
+                    <li v-else-if="i === 2">……</li>
+                </ul>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -34,6 +51,47 @@
 
 <style lang="scss" scoped>
     .routes {
-        
+        background: #ebebeb;
+        .routeslists {
+            .routeslist {
+                background: #FFF;
+                margin: 0.5em;
+                padding: 0.5em;
+                .detail {
+                    display: flex;
+                    align-items: center;
+                    .img {
+                        width: 100px;
+                        height: 100px;
+                        margin-right: 1em;
+                    }
+                    .desc {
+                        flex: 1;
+                        display: flex;
+                        flex-direction: column;
+                        align-items: flex-start;
+                        .title {
+
+                        }
+                        .days {
+                            color: #f2920c;
+                        }
+                        .nickname {
+
+                        }
+                    }
+                }
+                .destinations {
+                    margin: 1em 0;
+                    width: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    .destination {
+                        
+                        
+                    }
+                }
+            }
+        }
     }
 </style>
